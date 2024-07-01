dd524b4189facbf1f3d756ef42a1f36d33a89fa9
Javid <sairexm@gmail.com>
2024-07-01 22:03:24 +0400

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        }
+        Author stephenKing = new Author("Stephen","King");
+        Author ernestHemingway = new Author("Ernest","Hemingway");
+        Book misery = new Book("Misery", stephenKing, 1987);
+        Book inOurTime = new Book("In Our Time", ernestHemingway, 1923);
+        inOurTime.setDate(1924); //correct date
+    }
 }
